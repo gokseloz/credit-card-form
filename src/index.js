d021508b6a75421a65232052bed7b91633402fdb
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2022-01-12 09:38:34 +0100

    initial setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles/App.scss";
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<App />, rootElement);
